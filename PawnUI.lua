7511b8baf4912af11f681703d9505a14cb512816
Leon-Paul Hart <24394216+leon-paul-hart@users.noreply.github.com>
2020-10-13 14:39:19 +0100

    Updated the API call on line 2221 to GetQuestID() as advised
    Change based on feedback

--- PawnUI.lua
+++ PawnUI.lua
@@ -2218,7 +2218,7 @@ function PawnUI_OnQuestInfo_ShowRewards()
 	if not PawnCommon.ShowQuestUpgradeAdvisor then return end
 
 	-- Now, get information about this quest.
-	-- local QuestID = C_QuestLog.GetSelectedQuest()
+	local QuestID = GetQuestID()
 	local IsInMap = WorldMapFrame:IsShown()
 	local StaticRewards, RewardChoices
 	local GetLinkFunction, GetRewardInfoFunction, GetChoiceInfoFunction
