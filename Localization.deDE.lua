4d3cd542be75a391c159afd0117d1eaaf981b879
Travis Spomer <travis+github@greeneclipsesoftware.com>
2020-12-03 21:17:00 -0800

    Classic deDE mail armor translation fix

--- Localization.deDE.lua
+++ Localization.deDE.lua
@@ -614,9 +614,12 @@ Weitere Informationen zu dieser Einstellung findest du in der Readme-Datei.]=],
 	}
 }
 
--- Special case: weapon speed uses a different word on Classic.
+-- Special case: weapon speed and Mail use different words on Classic.
 -- So, patch things up here.
 if VgerCore.IsClassic then
+	PawnLocal.Mail = "Schwere Rüstung"
+	PawnLocal.MailInfo = "Punkte, die zugewiesen werden sollen, wenn der Gegenstand Schwere Rüstung ist."
+	PawnLocal.TooltipParsing.Mail = "^Schwere Rüstung$"
 	PawnLocal.TooltipParsing.Speed = "^Tempo #$"
 end
 
