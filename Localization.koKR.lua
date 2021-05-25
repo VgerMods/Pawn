6e217983dc796698bb25fcb8072c64f48b34b5ec
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-05-24 20:38:13 -0700

    Same wand fix for koKR

--- Localization.koKR.lua
+++ Localization.koKR.lua
@@ -668,7 +668,7 @@ Pawn이 상점에 판매했거나, 파괴했거나, 다른 방법으로 더이�
 
 -- Special case: wands actually use different text on live versus classic.
 -- So, patch things up here.
-if VgerCore.IsClassic then
+if VgerCore.IsClassic or VgerCore.IsBurningCrusade then
 
 	local TooltipParsing_Classic =
 	{
