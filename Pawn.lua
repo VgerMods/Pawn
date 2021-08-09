c7b449682993a3173e42c9c69ac792c11a7804e5
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-08-08 17:53:01 -0700

    Rev version to 2.5.24

--- Pawn.lua
+++ Pawn.lua
@@ -7,7 +7,7 @@
 -- Main non-UI code
 ------------------------------------------------------------
 
-PawnVersion = 2.0523
+PawnVersion = 2.0524
 
 -- Pawn requires this version of VgerCore:
 local PawnVgerCoreVersionRequired = 1.13
