e2c846fc00df409928104a03c507077b0d156175
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-05-11 22:27:30 -0700

    TBC phase 5 gems

--- ClassicHawsJon.lua
+++ ClassicHawsJon.lua
@@ -9,7 +9,7 @@
 
 local ScaleProviderName = "Classic"
 
-PawnClassicLastUpdatedVersion = 2.0521
+PawnClassicLastUpdatedVersion = 2.0538
 
 
 function PawnClassicScaleProvider_AddScales()
--- GemsBurningCrusade.lua
+++ GemsBurningCrusade.lua
@@ -158,7 +158,7 @@ local PawnGemDataRare =
 { ID = 24061, R = true, Y = true, Stats = { HitRating = 4, Agility = 4 } }, -- Glinting Noble Topaz
 { ID = 31867, R = true, Y = true, Stats = { SpellHitRating = 4, SpellDamage = 5 } }, -- Veiled Noble Topaz
 { ID = 31868, R = true, Y = true, Stats = { CritRating = 4, Ap = 8 } }, -- Wicked Noble Topaz
--- { ID = 35316, R = true, Y = true, Stats = { SpellHasteRating = 4, SpellDamage = 5 } }, -- Reckless Noble Topaz (PHASE 5)
+{ ID = 35316, R = true, Y = true, Stats = { SpellHasteRating = 4, SpellDamage = 5 } }, -- Reckless Noble Topaz (PHASE 5)
 
 
 ------------------------------------------------------------
@@ -172,7 +172,7 @@ local PawnGemDataRare =
 { ID = 24052, Y = true, Stats = { DefenseRating = 8 } }, -- Thick Dawnstone
 { ID = 24053, Y = true, Stats = { ResilienceRating = 8 } }, -- Mystic Dawnstone
 { ID = 31861, Y = true, Stats = { SpellHitRating = 8 } }, -- Great Dawnstone
--- { ID = 35315, Y = true, Stats = { SpellHasteRating = 8 } }, -- Quick Dawnstone (PHASE 5)
+{ ID = 35315, Y = true, Stats = { SpellHasteRating = 8 } }, -- Quick Dawnstone (PHASE 5)
 
 
 ------------------------------------------------------------
@@ -184,7 +184,7 @@ local PawnGemDataRare =
 { ID = 24066, Y = true, B = true, Stats = { SpellCritRating = 4, SpellPenetration = 5 } }, -- Radiant Talasite
 { ID = 24067, Y = true, B = true, Stats = { CritRating = 4, Stamina = 6 } }, -- Jagged Talasite
 { ID = 33782, Y = true, B = true, Stats = { ResilienceRating = 4, Stamina = 6 } }, -- Steady Talasite
--- { ID = 35318, Y = true, B = true, Stats = { SpellHasteRating = 4, Stamina = 6 } }, -- Forceful Talasite (PHASE 5)
+{ ID = 35318, Y = true, B = true, Stats = { SpellHasteRating = 4, Stamina = 6 } }, -- Forceful Talasite (PHASE 5)
 
 
 ------------------------------------------------------------
@@ -207,7 +207,7 @@ local PawnGemDataRare =
 { ID = 24057, R = true, B = true, Stats = { Healing = 9, SpellDamage = 3, Mp5 = 2 } }, -- Royal Nightseye
 { ID = 31863, R = true, B = true, Stats = { Ap = 8, Stamina = 6 } }, -- Balanced Nightseye
 { ID = 31865, R = true, B = true, Stats = { Ap = 8, Mp5 = 2 } }, -- Infused Nightseye
--- { ID = 35707, R = true, B = true, Stats = { DodgeRating = 4, Stamina = 6 } }, -- Regal Nightseye (PHASE 5)
+{ ID = 35707, R = true, B = true, Stats = { DodgeRating = 4, Stamina = 6 } }, -- Regal Nightseye (PHASE 5)
 
 
 }
@@ -257,7 +257,7 @@ local PawnGemDataEpic =
 { ID = 32208, Y = true, Stats = { DefenseRating = 10 } }, -- Thick Lionseye
 { ID = 32209, Y = true, Stats = { ResilienceRating = 10 } }, -- Mystic Lionseye
 { ID = 32210, Y = true, Stats = { SpellHitRating = 10 } }, -- Great Lionseye
--- { ID = 35761, Y = true, Stats = { SpellHasteRating = 10 } }, -- Quick Lionseye (PHASE 5)
+{ ID = 35761, Y = true, Stats = { SpellHasteRating = 10 } }, -- Quick Lionseye (PHASE 5)
 
 
 ------------------------------------------------------------
@@ -292,7 +292,7 @@ local PawnGemDataEpic =
 { ID = 32214, R = true, B = true, Stats = { Ap = 10, Mp5 = 2 } }, -- Infused Shadowsong Amethyst
 { ID = 32215, R = true, B = true, Stats = { SpellDamage = 6, Stamina = 7 } }, -- Glowing Shadowsong Amethyst
 { ID = 32216, R = true, B = true, Stats = { Healing = 11, SpellDamage = 4, Mp5 = 2 } }, -- Royal Shadowsong Amethyst
--- { ID = 37503, R = true, B = true, Stats = { Healing = 11, SpellDamage = 4, Spirit = 5 } }, -- Purified Shadowsong Amethyst (PHASE 5)
+{ ID = 37503, R = true, B = true, Stats = { Healing = 11, SpellDamage = 4, Spirit = 5 } }, -- Purified Shadowsong Amethyst (PHASE 5)
 
 
 }
@@ -313,8 +313,8 @@ local PawnMetaGemDataRare =
 { ID = 25890, Stats = { SpellCritRating = 14 } }, -- Destructive Skyfire Diamond
 { ID = 25894, Stats = { Ap = 24 } }, -- Swift Skyfire Diamond
 { ID = 34220, Stats = { SpellCritRating = 12 } }, -- Chaotic Skyfire Diamond
--- { ID = 35501, Stats = { DefenseRating = 12 } }, -- Eternal Earthstorm Diamond (PHASE 5)
--- { ID = 35503, Stats = { SpellDamage = 14 } }, -- Ember Skyfire Diamond (PHASE 5)
+{ ID = 35501, Stats = { DefenseRating = 12 } }, -- Eternal Earthstorm Diamond (PHASE 5)
+{ ID = 35503, Stats = { SpellDamage = 14 } }, -- Ember Skyfire Diamond (PHASE 5)
 
 
 }
