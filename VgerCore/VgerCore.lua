6ba55972bd98a784556008a78cc2ac85be1d627f
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-08-29 22:48:34 -0700

    Fix IsWrath on Burning Crusade

--- VgerCore/VgerCore.lua
+++ VgerCore/VgerCore.lua
@@ -2,7 +2,7 @@
 -- www.vgermods.com
 -- © 2006-2022 Travis Spomer.  This mod is released under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 license.
 -- 
--- Version 1.0.15 -- IsWrath, IsMainline, IsDragonflight
+-- Version 1.0.16 -- IsWrath should still work on Burning Crusade Classic
 local VgerCoreThisVersion = 1.15
 -- 
 -- VgerCore contains functionality that is shared by Vger's mods.
@@ -22,7 +22,7 @@ VgerCore.Version = VgerCoreThisVersion
 local BuildNumber = select(4, GetBuildInfo())
 VgerCore.IsClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 VgerCore.IsBurningCrusade = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC and LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_BURNING_CRUSADE) -- includes pre-patch
-VgerCore.IsWrath = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC and LE_EXPANSION_LEVEL_CURRENT >= LE_EXPANSION_WRATH_OF_THE_LICH_KING) -- includes pre-patch
+VgerCore.IsWrath = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC and LE_EXPANSION_WRATH_OF_THE_LICH_KING and LE_EXPANSION_LEVEL_CURRENT >= LE_EXPANSION_WRATH_OF_THE_LICH_KING) -- includes pre-patch
 VgerCore.IsMainline = BuildNumber >= 90000
 VgerCore.IsDragonflight = VgerCore.IsMainline and BuildNumber >= 100000
 VgerCore.IsShadowlands = VgerCore.IsMainline and BuildNumber >= 90000
