8f6296be0feb7b0f136e59b38128b148784f91dc
spiffy46 <spiffy146@yahoo.com>
2022-11-17 12:49:20 -0700

    Update Pawn.lua
    GetContainerItemInfo has moved to C_Container

--- Pawn.lua
+++ Pawn.lua
@@ -419,7 +419,7 @@ function PawnInitialize()
 		PawnOriginalIsContainerItemAnUpgrade = IsContainerItemAnUpgrade
 		PawnIsContainerItemAnUpgrade = function(bagID, slot, ...)
 			if PawnCommon.ShowBagUpgradeAdvisor then
-				local _, Count, _, _, _, _, ItemLink = GetContainerItemInfo(bagID, slot)
+				local _, Count, _, _, _, _, ItemLink = C_Container.GetContainerItemInfo(bagID, slot)
 				if not Count then return false end -- If the stack count is 0, it's clearly not an upgrade
 				if not ItemLink then return nil end -- If we didn't get an item link, but there's an item there, try again later
 				return PawnShouldItemLinkHaveUpgradeArrow(ItemLink, true) -- true means to check player level
