3956712a96b9e24551e91863f90e8c9cc6d6fa45
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-11-18 09:07:38 -0800

    Update Pawn.lua

--- Pawn.lua
+++ Pawn.lua
@@ -419,12 +419,10 @@ function PawnInitialize()
 		PawnOriginalIsContainerItemAnUpgrade = IsContainerItemAnUpgrade
 		PawnIsContainerItemAnUpgrade = function(bagID, slot, ...)
 			if PawnCommon.ShowBagUpgradeAdvisor then
-				local cii = C_Container.GetContainerItemInfo(bagID, slot)
-				local Count, ItemLink = cii.stackCount, cii.hyperlink
-				--VgerCore.Message(tostring(Count) .. " " .. tostring(ItemLink))
-				if not Count then return false end -- If the stack count is 0, it's clearly not an upgrade
-				if not ItemLink then return nil end -- If we didn't get an item link, but there's an item there, try again later
-				return PawnShouldItemLinkHaveUpgradeArrow(ItemLink, true) -- true means to check player level
+				local ItemInfo = C_Container.GetContainerItemInfo(bagID, slot)
+				if not ItemInfo.stackCount then return false end -- If the stack count is 0, it's clearly not an upgrade
+				if not ItemInfo.hyperlink then return nil end -- If we didn't get an item link, but there's an item there, try again later
+				return PawnShouldItemLinkHaveUpgradeArrow(ItemInfo.hyperlink, true) -- true means to check player level
 			else
 				---@diagnostic disable-next-line: redundant-parameter
 				return PawnOriginalIsContainerItemAnUpgrade(bagID, slot, ...)
