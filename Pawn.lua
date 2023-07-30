09d0f96078f0338029a3af80f1266642b17f761a
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2023-07-30 15:04:52 -0700

    Budgeted PawnShouldItemLinkHaveUpgradeArrow

--- Pawn.lua
+++ Pawn.lua
@@ -5779,18 +5779,40 @@ function PawnIsItemIDAnUpgrade(ItemID)
 	return PawnIsItemAnUpgrade(Item)
 end
 
--- This is largely the same as getting the item data for a link and then calling PawnIsItemAnUpgrade on it,
--- but this one also works with relics, can support minimum level requirements, and so on.  It's intended as the
--- easiest way to answer the question "should this item have a green arrow?".
+-- Returns a wrapper function that allows an inner function to spend a certain amount of time executing
+-- over a specific period. Once the time budget is exceeded, it returns nil until the full period has elapsed
+-- and the budget resets.
+-- Parameters:
+-- 	Func: The function to call. This function can take arguments.
+-- 	Budget: The maximum amount of time to spend, in seconds.
+-- 	Period: How often the budget resets, in seconds.
 -- Returns:
---   true: This item is indeed an upgrade for something.
---   false: This item is not an upgrade.
---   nil: We're not sure yet.
-function PawnShouldItemLinkHaveUpgradeArrow(ItemLink, CheckLevel, EvenOnClassic)
-	-- TEMPORARY HACK: If EvenOnClassic=true wasn't passed in and this is a Classic version, just return false.
-	-- This intentionally breaks integration with bag addons for now to resolve performance issues.
-	if (not EvenOnClassic) and (not VgerCore.IsMainline) then return false end
+-- 	A new function wrapping Func that starts immediately returning nil instead of executing whenever over its time budget.
+-- Example:
+-- 	ThrottledHello = BudgetThrottle(function() print("Hello") end, 1 / 60, 1)
+-- 	ThrottledHello() stops printing Hello once it has used 1/60th of each second, capping it at spending 1 fps.
+local function BudgetThrottle(Func, Budget, Period)
+	local PeriodEndsAt = 0
+	local RemainingBudget = 0
+	local Throttled = function(...)
+		local Now = GetTimePreciseSec()
+		if Now >= PeriodEndsAt then
+			-- Period expired, so reset the budget..
+			PeriodEndsAt = Now + Period
+			RemainingBudget = Budget
+		elseif RemainingBudget <= 0 then
+			-- We ran ot of budget, so keep skipping the function until we get our next allowance.
+			return nil
+		end
+		local AllReturns = {Func(...)}
+		local Elapsed = GetTimePreciseSec() - Now
+		RemainingBudget = RemainingBudget - Elapsed
+		return unpack(AllReturns)
+	end
+	return Throttled
+end
 
+function PawnShouldItemLinkHaveUpgradeArrowUnbudgeted(ItemLink, CheckLevel)
 	if not PawnIsInitialized then VgerCore.Fail("Can't check to see if items are upgrades until Pawn is initialized") return end
 
 	--if PawnOptions.DebugBagArrows then VgerCore.Message("Checking upgrade information for " .. tostring(ItemLink)) end
@@ -5823,6 +5845,16 @@ function PawnShouldItemLinkHaveUpgradeArrow(ItemLink, CheckLevel, EvenOnClassic)
 	end
 end
 
+-- This is largely the same as getting the item data for a link and then calling PawnIsItemAnUpgrade on it,
+-- but this one also works with relics, can support minimum level requirements, and so on.  It's intended as the
+-- easiest way to answer the question "should this item have a green arrow?".
+-- Calculations are budgeted at 2 FPS out of 60, resetting every 1/4 of a second.
+-- Returns:
+--   true: This item is indeed an upgrade for something.
+--   false: This item is not an upgrade.
+--   nil: We're not sure yet.
+PawnShouldItemLinkHaveUpgradeArrow = BudgetThrottle(PawnShouldItemLinkHaveUpgradeArrowUnbudgeted, 2 / 60 / 4, 1 / 4)
+
 -- Clears the best item level data for this character only.
 function PawnClearBestItemLevelData()
 	PawnOptions.ItemLevels = nil
@@ -5835,7 +5867,7 @@ end
 -- Shows or hides the Pawn UI.
 function PawnUIShow()
 	if not PawnIsInitialized or not PawnUIFrame then
-		VgerCore.Fail("Pawn UI is not loaded!")
+		VgerCore.Fail("There was an error loading Pawn and its UI is not ready. /console scriptErrors 1 can help you see why.")
 		return
 	end
 	if PawnUIFrame:IsShown() then
--- PawnUI.lua
+++ PawnUI.lua
@@ -2287,7 +2287,7 @@ function PawnUI_LootHistoryFrame_UpdateItemFrame(self, ItemFrame, ...)
 	if ItemLink == nil then return end
 
 	-- Is this item an upgrade?
-	local IsUpgrade = PawnCommon.ShowLootUpgradeAdvisor and PawnShouldItemLinkHaveUpgradeArrow(ItemLink, false, true)
+	local IsUpgrade = PawnCommon.ShowLootUpgradeAdvisor and PawnShouldItemLinkHaveUpgradeArrowUnbudgeted(ItemLink, false)
 	if IsUpgrade then
 		-- If the arrow hasn't already been created, create it.
 		if not ItemFrame.PawnLootAdvisorArrow then
@@ -2311,7 +2311,7 @@ function PawnUI_LootWonAlertFrame_SetUp(self, ItemLink, ...)
 
 	-- Is this item an upgrade?
 	if ItemLink == nil then return end
-	local IsUpgrade = PawnCommon.ShowLootUpgradeAdvisor and PawnShouldItemLinkHaveUpgradeArrow(ItemLink, false, true)
+	local IsUpgrade = PawnCommon.ShowLootUpgradeAdvisor and PawnShouldItemLinkHaveUpgradeArrowUnbudgeted(ItemLink, false)
 
 	if IsUpgrade then
 		-- If the arrow hasn't already been created, create it.
