0f9d0676d8c57e51538f97041285560321a2cb0f
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2026-01-09 09:10:30 -0800

    Surprise judo version change!

--- Pawn.lua
+++ Pawn.lua
@@ -7,7 +7,7 @@
 -- Main non-UI code
 ------------------------------------------------------------
 
-PawnVersion = 2.1202
+PawnVersion = 2.1300
 
 -- Pawn requires this version of VgerCore:
 local PawnVgerCoreVersionRequired = 1.20
